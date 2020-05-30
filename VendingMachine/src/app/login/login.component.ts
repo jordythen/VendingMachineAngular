a851672c2a6298b186b0be2d22efb4d085e8e84a
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-05-30 17:49:40 -0400

    started login form

--- VendingMachine/src/app/login/login.component.ts
+++ VendingMachine/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
   }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches)
+  );
 
+  if(isHandset$){ //mobile view
+  }
 }
