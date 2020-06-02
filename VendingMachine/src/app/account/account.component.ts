f44bdd2c13405dd692fabda72cfe98a231d2adf1
Ryan Gerold <ryan.gerold@gmail.com>
2020-06-02 11:59:28 -0400

    saved project

--- VendingMachine/src/app/account/account.component.ts
+++ VendingMachine/src/app/account/account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { User } from '../classes/user';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  loggedUser: User;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  money: number;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.firstName = this.loggedUser.firstName;
+    this.lastName = this.loggedUser.lastName;
+    this.username = this.loggedUser.username;
+    this.password = this.loggedUser.password;
+    this.email = this.loggedUser.email;
+    this.money = this.loggedUser.balance;
   }
-
 }
